8f21a71a7de00331c2ec92b265ce04f9faeda6e5
Egor Komov <ekomov2006@gmail.com>
2025-12-30 16:47:03 +0300

    Correction of task 7

--- js/task7.js
+++ js/task7.js
@@ -3,6 +3,6 @@ function FalseValues(arr) {
 }
 
 console.log('Задача 7');
-const array1 = [0, 1, false, 2, '', 3, null, 'hello', undefined, NaN, 4, -0, 0n, document.all, 5];
+const array1 = [0, 1, false, 2, '', 3, null, 'hello', undefined, NaN, 4, -0, 0n, 5];
 const truthyValues = FalseValues(array1);
 console.log(truthyValues);
