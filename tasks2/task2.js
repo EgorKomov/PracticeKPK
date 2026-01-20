2bd9df8bf3e336208a7eb062798753c0471eebda
EgorKomov <156846662+EgorKomov@users.noreply.github.com>
2026-01-20 03:06:15 +0300

    Delete tasks2 directory

--- tasks2/task2.js
+++
@@ -1,7 +0,0 @@
-function noDuplicate(arr) {
-    return[...new Set(arr)];
-}
-
-
-const arr = [1, 2, 3, 4, 4, 5, 6, 1]
-console.log(noDuplicate(arr));
--- tasks2/task5.js
+++
@@ -1,8 +0,0 @@
-function isPalindrom(str) {
-    const reversedStr = str.split('').reverse().join('');
-
-    return reversedStr === str;
-}
-
-console.log(isPalindrom("hello"))
-console.log(isPalindrom("ded"))
