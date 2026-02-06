c024b6ab01115653cd9a30a7133d51821ba4729a
Egor Komov <ekomov2006@gmail.com>
2026-02-06 12:25:54 +0300

    JWT

--- src/app/routes/routesConfig.js
+++ src/app/routes/routesConfig.js
@@ -3,27 +3,31 @@ import { AuthPage } from "../../pages/Auth";
 import { RegPage } from "../../pages/Reg";
 import { DashboardPage } from "../../pages/Dashboard";
 import { CreateDashboardPage } from "../../pages/CreateDashboard";
-import { ENUM_LINK } from "../../shared/constants"
+import { ProtectedRoute } from "../../components/ProtectedRoute";
 
 export const routesConfig = createBrowserRouter([
-    {
-        children:[
-            {
-                path: ENUM_LINK.MAIN,
-                element: <AuthPage/>
-            },
-            {
-                path: ENUM_LINK.REG,
-                element: <RegPage/>
-            },
-            {
-                path: ENUM_LINK.DASHBOARD,
-                element: <DashboardPage/>
-            },
-            {
-                path: ENUM_LINK.CREATEDASHBOARD,
-                element: <CreateDashboardPage/>
-            }
-        ]
-    }
+  {
+    path: "/",
+    element: <AuthPage />
+  },
+  {
+    path: "/register",
+    element: <RegPage />
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <ProtectedRoute>
+        <DashboardPage />
+      </ProtectedRoute>
+    )
+  },
+  {
+    path: "/create-dashboard",
+    element: (
+      <ProtectedRoute>
+        <CreateDashboardPage />
+      </ProtectedRoute>
+    )
+  }
 ]);
